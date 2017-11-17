8064a764a68f6324481acbc6cd71b84fbdd63ade
Emilien <emilien.pilloud@epfl.ch>
2017-11-17 13:00:42 -0800

    Changed features argument to list for flexibility

--- src/main/scala/SmilePlotting.scala
+++ src/main/scala/SmilePlotting.scala
@@ -4,25 +4,11 @@ import smile.plot._
 
 
 object SmilePlotting {
-  def scatterPlot(features : (Int, Int), data : RDD[(String, Gse)], classes : Array[String]) : Window = {
+  def featurePlot(features : List[Int] , data : RDD[(String, Gse)], classes : Array[String]) : Window = {
     val formatted : Array[(Array[Array[Double]], Array[Int])] = for{
       cl <- classes
       x = data.filter{case(str, _) => str.equals(cl)}.values
-        .map(gse => Array(gse.geneData(features._1).toDouble, gse.geneData(features._2).toDouble))
-        .collect
-    } yield (x, Array.fill[Int](x.length)(classes.indexOf(cl)))
-
-    val genesCoor : Array[Array[Double]] = formatted.map(_._1).reduce(_ ++ _)
-    val classe : Array[Int] = formatted.map(_._2).reduce(_ ++ _)
-
-    plot(genesCoor, classe, Array('+', 'o'), Array(Color.red, Color.blue))
-  }
-
-  def scatterPlot(features : (Int, Int, Int), data : RDD[(String, Gse)], classes : Array[String]) : Window = {
-    val formatted : Array[(Array[Array[Double]], Array[Int])] = for{
-      cl <- classes
-      x = data.filter{case(str, _) => str.equals(cl)}.values
-        .map(gse => Array(gse.geneData(features._1).toDouble, gse.geneData(features._2).toDouble, gse.geneData(features._3).toDouble))
+        .map(gse => gse.geneData.zipWithIndex.filter{case (_, idx) => features.contains(idx)}.unzip._1.map(_.toDouble))
         .collect
     } yield (x, Array.fill[Int](x.length)(classes.indexOf(cl)))
 
